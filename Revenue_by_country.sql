a14d30cac1e2ed5228e7d1ca4b05094378466239
MabelChan2023 <141039280+MabelChan2023@users.noreply.github.com>
2023-08-12 22:05:29 -0400

    Add files via upload

---
+++ Revenue_by_country.sql
@@ -0,0 +1,8 @@
+SELECT co.country AS country, SUM(p.amount)AS total_revenue
+ FROM payment p
+ JOIN customer c ON c.customer_id = p.customer_id
+ JOIN address a ON a.address_id = c.address_id
+ JOIN city ci ON ci.city_id = a.city_id
+ JOIN country co ON co.country_id = ci.country_id
+ GROUP BY co.country
+ ORDER BY total_revenue DESC
